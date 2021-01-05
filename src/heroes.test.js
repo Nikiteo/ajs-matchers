db2124c94fedda4b84d7eec958003c6d5b5cae40
Nikiteo <nzhigiley@yandex.ru>
2021-01-05 11:26:21 +0300

    Add check not.toBe

--- src/heroes.test.js
+++ src/heroes.test.js
@@ -1,19 +1,45 @@
 import sortHeroes from './heroes.js';
 
-test('sort heroes from max health to min', () => {
-  const expected = [
+const expected = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
+const received = sortHeroes(
+  [
+    { name: 'мечник', health: 10 },
     { name: 'маг', health: 100 },
     { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ];
+  ],
+);
 
-  const received = sortHeroes(
+test('sort heroes from max health to min', () => {
+  expect(received).toEqual(expected);
+});
+
+test('sort heroes from max health to min', () => {
+  const result = sortHeroes(
     [
       { name: 'мечник', health: 10 },
       { name: 'маг', health: 100 },
       { name: 'лучник', health: 80 },
+      { name: 'рыцарь', health: 0 },
+      { name: 'гном', health: 100500 },
     ],
   );
 
-  expect(received).toEqual(expected);
+  const exp = [
+    { name: 'гном', health: 100500 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'рыцарь', health: 0 },
+  ];
+
+  expect(result).toEqual(exp);
+});
+
+test('sortHeroes: Not toBe', () => {
+  expect(sortHeroes(expected)).not.toBe(received);
 });
